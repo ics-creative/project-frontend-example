b2876ff29e42653a7ad4de6619c73f1893994c8c
ics-nishihara <nishihara@ics-web.jp>
2023-08-08 17:20:22 +0900

    テストコードの修正

--- utils/commaToNum.test.ts
+++ utils/commaToNum.test.ts
@@ -1,9 +1,9 @@
 import { commaToNum } from "./commaToNum";
 
 test("カンマ区切りの5桁数字の文字列を数値に直す", () => {
-  expect(commaToNum("30,000")).toEqual(30000);
+  expect(commaToNum("30,000")).toBe(30000);
 });
 
 test("カンマ区切りの8桁の数字の文字列を数値に直す", () => {
-  expect(commaToNum("15,000,000")).toEqual(15000000);
+  expect(commaToNum("15,000,000")).toBe(15000000);
 });
