9909de1045e6b3946b483643c1854497726f2f67
m-hamanaka <hamanaka@creativebank.co.jp>
2024-01-11 12:18:10 +0900

    修正対応

--- utils/commaToNum.ts
+++ utils/commaToNum.ts
@@ -2,6 +2,6 @@
  * カンマ区切りの文字列を数値に変換します
  */
 export const commaToNum = (str: string) => {
-  const noCommaStr = str.replace(",", "");
+  const noCommaStr = str.replaceAll(",", "");
   return Number(noCommaStr);
 };
